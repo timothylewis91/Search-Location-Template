10eb1ac584cacc3a2a63c04cf933854d37a50b4a
Timothy Lewis <99374704+timothylewis91@users.noreply.github.com>
2024-11-26 09:56:19 -0800

    Add files via upload

---
+++ LocationSearch.tsx
@@ -0,0 +1,46 @@
+import type {Place} from '../api/Place';
+import { Fragment, useState } from 'react';
+import {search} from '../api/Search';
+
+interface  LocationSearchProps {
+    onPlaceClick: (place: Place) => void;
+
+
+}
+export default function LocationSearch ({onPlaceClick}: LocationSearchProps){
+    const [places, setPlaces] = useState<Place[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+        const results = await search(searchTerm);
+    setPlaces(results);
+    };
+
+    return <div> 
+        <form onSubmit={handleSubmit}>
+            <label className='font-bold' htmlFor='term'>Search </label>
+            <input className= "border border-gray-300 rounded-md shadow-sm focus: border-indigo-500 px-4 py-2 w-full" 
+            id="searchTerm"
+            value={searchTerm}
+            onChange={e=> setSearchTerm(e.target.value)}
+            />
+            
+
+        </form>
+        <h1 className="font-bold mt-6">Found Locations</h1>
+        <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+            {
+                places.map(place => {
+                    return <Fragment key={place.id}>
+                        <p className="text-sm">{place.name}</p>
+                        <button className="bg-blue-500 text-xs text-white font bold py-1 px-1 rounded"
+                        onClick={() => onPlaceClick(place)}>
+                            Go
+                        </button>
+                        <div className="border-b w-full col-span-2"/>
+                    </Fragment>
+                })
+            }
+        </div>
+    </div>
+}
